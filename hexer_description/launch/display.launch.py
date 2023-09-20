a6cf560d90ffa00e6faee40f82fc813859dfd7d7
Vendicus <mich.aksamit@wp.pl>
2023-09-20 11:42:24 +0200

    urdf created and vision upgrade

---
+++ hexer_description/launch/display.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(get_package_share_path('hexer_description'),
+                            'urdf','Hexer_v_1_0.urdf.xacro')
+    
+    rviz_config_path = os.path.join(get_package_share_path('hexer_description'),
+                            'rviz','rviz_conf_Hexer_v_1_0.rviz')
+
+    robot_description =  ParameterValue(Command(['xacro ',urdf_path]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description' : robot_description}]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz2_node
+    ])
--- vision_rpi_hexabot/vision_rpi_hexabot/object_detection.py
+++ vision_rpi_hexabot/vision_rpi_hexabot/object_detection.py
@@ -3,8 +3,10 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+
 #import tflite_runtime.interpreter as tflite 
 
+import time
 import cv2
 import numpy as np
 
@@ -30,19 +32,28 @@ class camera_sub(Node):
         with open(self.label_file,'rt') as f:
             self.label = f.read().rstrip('\n').split('\n')
 
+        self.start_time = 0
+
     def camera_rec(self, msg):
+        #calculate time for fps
+        currrent_time = time.time()
+        fps = 1/(currrent_time - self.start_time)
+        self.start_time = currrent_time
+
         #brdige between rpi and computer to cath image 
         frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
         frame = frame.astype(np.uint8) # convert frame to format ready to read by AI
     
         # data for rectangle, labels, all obejcts
-        label_ID, conf, bbox = self.detector.detect(frame, confThreshold = 0.55)
+        label_ID, conf, bbox = self.detector.detect(frame, confThreshold = 0.65)
 
         #creating boxes and labels on vision for all detected objects
         for class_ID, confidence, border in zip(np.array(label_ID).flatten(),np.array(conf).flatten(),bbox):
-            if confidence < 0.60:
+            
+
+            if confidence < 0.70:
                 box_color = (55,0,255)
-            elif confidence >= 0.60 and confidence <= 0.70:
+            elif confidence >= 0.70 and confidence <= 0.80:
                 box_color = (20,220,232)
             else :
                 box_color = (52,235,28)
@@ -58,6 +69,7 @@ class camera_sub(Node):
             cv2.putText(frame, str("PROB: ") + str(round(confidence, 2)),(border[0] + x, border[3] + border[1] - 10), cv2.FONT_HERSHEY_TRIPLEX, 0.3, box_color ,1)
         
         frame = cv2.resize(frame, (1280,720), interpolation = cv2.INTER_AREA)
+        cv2.putText(frame,"FPS : " + str(int(fps)), (20,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2) # fps text representation
         cv2.imshow('Vision from AI',frame)
         cv2.waitKey(1)
 
